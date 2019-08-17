44fbbc46e4f7c00822843a83eff4373dae205cb4
Andrew Kane <andrew@chartkick.com>
2019-08-16 22:55:46 -0700

    Minimum makefile

--- ext/xgb/extconf.rb
+++ ext/xgb/extconf.rb
@@ -1,28 +1,9 @@
 require "mkmf"
 
-$dir = File.expand_path("../../vendor/xgboost/build", __dir__)
+dir = File.expand_path("../../vendor/xgboost", __dir__)
 
-begin
-  Dir.mkdir($dir)
-rescue Errno::EEXIST
-  # already exists
-end
-
-def run(command)
-  puts ">> #{command}"
-  # cd here to prevent warning
-  unless system("cd #{$dir} && #{command}")
-    raise "Command failed"
-  end
-end
-
-arch = RbConfig::CONFIG["arch"]
-if arch =~ /darwin/i
-  $dir = File.expand_path("../../vendor/xgboost", __dir__)
+Dir.chdir(dir) do
   run "cp make/minimum.mk config.mk"
-  run "make -j4"
-else
-  run "cmake .."
   run "make -j4"
 end
 
