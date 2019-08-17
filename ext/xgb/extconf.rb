783822265207551a56e41a93012cbea585ae8132
Andrew Kane <andrew@chartkick.com>
2019-08-16 23:08:50 -0700

    Make minimum [skip ci]

--- ext/xgb/extconf.rb
+++ ext/xgb/extconf.rb
@@ -10,7 +10,7 @@ end
 dir = File.expand_path("../../vendor/xgboost", __dir__)
 
 Dir.chdir(dir) do
-  run "cp make/config.mk config.mk"
+  run "cp make/minimum.mk config.mk"
   run "make -j4"
 end
 
