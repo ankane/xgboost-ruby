6bcd771b198b7d310de0ca1740ade6c457f6808a
Andrew Kane <andrew@chartkick.com>
2019-08-16 21:15:43 -0700

    WIP [skip ci]

--- ext/xgb/extconf.rb
+++ ext/xgb/extconf.rb
@@ -9,7 +9,7 @@ rescue Errno::EEXIST
 end
 
 def run(command)
-  puts command
+  puts ">> #{command}"
   # cd here to prevent warning
   unless system("cd #{$dir} && #{command}")
     raise "Command failed"
@@ -18,7 +18,7 @@ end
 
 arch = RbConfig::CONFIG["arch"]
 if arch.end_with?("-darwin18")
-  run 'CC=gcc-8 CXX=g++-8 cmake -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_C_LIB_NAMES="omp" -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=$(brew --prefix libomp)/lib/libomp.dylib ..'
+  run 'CC=gcc-8 CXX=g++-8 cmake -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_C_LIB_NAMES="omp" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=$(brew --prefix libomp)/lib/libomp.dylib ..'
 elsif arch =~ /darwin/i
   run "CC=gcc-8 CXX=g++-8 cmake .."
 else
