1596e44d43573a12614eb2d01830bc734076f7e2
Andrew Kane <andrew@chartkick.com>
2019-08-16 21:09:42 -0700

    Fix cd warning [skip ci]

--- ext/xgb/extconf.rb
+++ ext/xgb/extconf.rb
@@ -1,30 +1,29 @@
 require "mkmf"
 
-dir = File.expand_path("../../vendor/xgboost/build", __dir__)
+$dir = File.expand_path("../../vendor/xgboost/build", __dir__)
 
 begin
-  Dir.mkdir(dir)
+  Dir.mkdir($dir)
 rescue Errno::EEXIST
   # already exists
 end
 
 def run(command)
   puts command
-  unless system(command)
+  # cd here to prevent warning
+  unless system("cd #{$dir} && #{command}")
     raise "Command failed"
   end
 end
 
-Dir.chdir(dir) do
-  arch = RbConfig::CONFIG["arch"]
-  if arch.end_with?("-darwin18")
-    run 'CC=gcc-8 CXX=g++-8 cmake -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_C_LIB_NAMES="omp" -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=$(brew --prefix libomp)/lib/libomp.dylib ..'
-  elsif arch =~ /darwin/i
-    run "CC=gcc-8 CXX=g++-8 cmake .."
-  else
-    run "cmake .."
-  end
-  run "make -j4"
+arch = RbConfig::CONFIG["arch"]
+if arch.end_with?("-darwin18")
+  run 'CC=gcc-8 CXX=g++-8 cmake -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_C_LIB_NAMES="omp" -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=$(brew --prefix libomp)/lib/libomp.dylib ..'
+elsif arch =~ /darwin/i
+  run "CC=gcc-8 CXX=g++-8 cmake .."
+else
+  run "cmake .."
 end
+run "make -j4"
 
 puts "done"
