aea5e439f09dc1e545ec5f0903134ebaee328a0d
Andrew Kane <andrew@chartkick.com>
2019-08-16 23:18:01 -0700

    Different architectures [skip ci]

--- ext/xgb/extconf.rb
+++ ext/xgb/extconf.rb
@@ -9,9 +9,27 @@ end
 
 dir = File.expand_path("../../vendor/xgboost", __dir__)
 
-Dir.chdir(dir) do
-  run "cp make/minimum.mk config.mk"
-  run "make -j4"
+arch = RbConfig::CONFIG["arch"]
+puts "Arch: #{arch}"
+case arch
+when /darwin/i
+  Dir.chdir(dir) do
+    # doesn't support:
+    # - openmp (multicore)
+    # - rabit (distributed)
+    run "cp make/minimum.mk config.mk"
+    run "make -j4"
+  end
+when /mingw/
+  Dir.chdir(dir) do
+    run "cp make/mingw64.mk config.mk"
+    run "make -j4"
+  end
+else
+  Dir.chdir("#{dir}/build") do
+    run "cmake .."
+    run "make -j4"
+  end
 end
 
 create_makefile("xgb")
