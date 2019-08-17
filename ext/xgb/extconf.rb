2438f36e6104ed3ea44b764e1df64d425afa0365
Andrew Kane <andrew@chartkick.com>
2019-08-16 21:00:36 -0700

    Updated command [skip ci]

--- ext/xgb/extconf.rb
+++ ext/xgb/extconf.rb
@@ -17,13 +17,7 @@ end
 Dir.chdir(dir) do
   arch = RbConfig::CONFIG["arch"]
   if arch.end_with?("-darwin18")
-    run 'cmake
--DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" \
--DOpenMP_C_LIB_NAMES="omp" \
--DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" \
--DOpenMP_CXX_LIB_NAMES="omp" \
--DOpenMP_omp_LIBRARY=$(brew --prefix libomp)/lib/libomp.dylib \
-    ..'
+    run 'CC=gcc-8 CXX=g++-8 cmake -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_C_LIB_NAMES="omp" -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I$(brew --prefix libomp)/include" -DOpenMP_CXX_LIB_NAMES="omp" -DOpenMP_omp_LIBRARY=$(brew --prefix libomp)/lib/libomp.dylib ..'
   elsif arch =~ /darwin/i
     run "CC=gcc-8 CXX=g++-8 cmake .."
   else
