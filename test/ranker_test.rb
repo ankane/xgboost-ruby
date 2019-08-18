ee10d0dcfcae019431eeaddfb0403606ec8b5b45
Andrew Kane <andrew@chartkick.com>
2019-08-18 04:17:19 -0700

    Debug ranker

--- test/ranker_test.rb
+++ test/ranker_test.rb
@@ -8,6 +8,7 @@ class RankerTest < Minitest::Test
     model = Xgb::Ranker.new
     model.fit(x_train, y_train, group)
     y_pred = model.predict(x_test)
+    p y_pred[0, 6]
     expected = [3.690385, 4.999046, -3.8156319, 0.61984086, 0.30764353, 0.6986507]
     assert_elements_in_delta expected, y_pred[0, 6]
 
@@ -25,6 +26,7 @@ class RankerTest < Minitest::Test
     model = Xgb::Ranker.new
     model.fit(x_train, y_train, group)
 
+    p model.feature_importances
     expected = [0.04503533, 0.06504705, 0.55673695, 0.33318064]
     assert_elements_in_delta expected, model.feature_importances
   end
