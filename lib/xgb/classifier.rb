52856ddd2ab1f4cd3c464494218c9c76cd793907
Andrew Kane <andrew@chartkick.com>
2019-08-19 03:16:00 -0700

    Fixed rubocop warning [skip ci]

--- lib/xgb/classifier.rb
+++ lib/xgb/classifier.rb
@@ -31,7 +31,7 @@ module Xgb
       if y_pred.first.is_a?(Array)
         # multiple classes
         y_pred.map do |v|
-          v.map.with_index.max_by { |v2, i| v2 }.last
+          v.map.with_index.max_by { |v2, _| v2 }.last
         end
       else
         y_pred.map { |v| v > 0.5 ? 1 : 0 }
